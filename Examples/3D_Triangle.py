6c3286064083404e4f4d5439ee403b39ef6babac
rChantx86 <91958897+rChantx86@users.noreply.github.com>
2021-11-23 13:27:08 +0000

    Update 3D_Triangle.py

--- Examples/3D_Triangle.py
+++ Examples/3D_Triangle.py
@@ -15,7 +15,6 @@ def main():
 	while True:
 
 		window.clear()  # erase screen data
-		rascii.clear_console()  # clear actual console
 
 		x1 = round(r * math.cos(t)) + int(window.width/2)
 		y1 = round(r * math.sin(t)) + int(window.height/2)
@@ -26,12 +25,12 @@ def main():
 		x3 = round(r * math.cos(t+((8/3)*math.pi))) + int(window.width/2)
 		y3 = round(r * math.sin(t+((5/3)*math.pi))) + int(window.height/2)
 
-		x4 = round(r * math.cos(t+((25/3)*math.pi))) + int(window.width/2)
-		y4 = round(r * math.sin(t+((10/3)*math.pi))) + int(window.height/2)
+		x4 = round(r * math.cos(t+((12/3)*math.pi))) + int(window.width/2)
+		y4 = round(r * math.sin(t+((9/3)*math.pi))) + int(window.height/2)
 
 		rascii.color = '*'
 		rascii.triangle(window, x1, y1, x2, y2, x3, y3, True)
-		rascii.triangle(window, x1, y1, x3, y3, x4, y4, True)
+		rascii.triangle(window, x1, y1, x2, y2, x4, y4)
 		rascii.triangle(window, x3, y3, x2, y2, x4, y4, True)
 
 		t += 0.04
@@ -41,3 +40,41 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+# Must run in command prompt
+# No Ending yet so use "ctrl + c" to stop
+# Delete the guide if you don't need it ⬇️
+
+
+###############
+#    Guide    #
+###############
+"""
+Color:
+	rascii.color = '{character}'
+
+Fill:
+	rascii.fill = True/False
+	rascii.fill_color = '{character}'
+
+Translate:
+	rascii.translateVector = [xtranslation, ytranslation]
+
+Point:
+	rascii.point(window, x, y)
+
+Circle:
+	rascii.circle(window, x, y, radius, Transparent=False/True)
+
+Line:
+	rascii.line(window, x1, y1, x2, y2)
+
+Rectangle:
+	rascii.rect(window, x, y, width, height, Transparent=False/True)
+
+Triangle:
+	rascii.triangle(window, x1, y1, x2, y2, x3, y3, Transparen=False/True)
+
+Shape:
+	rascii.shape(window, point_data, connected=True/False)
+"""
